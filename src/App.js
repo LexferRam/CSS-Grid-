d9eaec0fa8f5a7bf35d609971b22ff31dfe93d97
Lexfer <lexferramirez@gmail.com>
2021-11-20 15:34:27 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,165 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+     <div className="wrapper">
+      <div>
+        Tempor voluptate nisi voluptate ullamco aliquip ut velit culpa labore incididunt laboris. Amet laborum reprehenderit dolore aute. 
+      </div>
+      <div>
+        Tempor voluptate nisi voluptate ullamco aliquip ut velit culpa labore incididunt laboris. Amet laborum reprehenderit dolore aute. 
+      </div>
+      <div className='nested'>
+        Tempor voluptate nisi voluptate ullamco aliquip ut velit culpa labore incididunt laboris. Amet laborum reprehenderit dolore aute. 
+      </div>
+      <div>
+        Tempor voluptate nisi voluptate ullamco aliquip ut velit culpa labore incididunt laboris. Amet laborum reprehenderit dolore aute. 
+      </div>
+      <div>
+        Tempor voluptate nisi voluptate ullamco aliquip ut velit culpa labore incididunt laboris. Amet laborum reprehenderit dolore aute. 
+      </div>
+      <div>
+        Tempor voluptate nisi voluptate ullamco aliquip ut velit culpa labore incididunt laboris. Amet laborum reprehenderit dolore aute. 
+      </div>
+      <div>
+        Tempor voluptate nisi voluptate ullamco aliquip ut velit culpa labore incididunt laboris. Amet laborum reprehenderit dolore aute. 
+      </div>
+      <div>
+        Tempor voluptate nisi voluptate ullamco aliquip ut velit culpa labore incididunt laboris. Amet laborum reprehenderit dolore aute. 
+      </div>
     </div>
+
+      {/* <div className="wrapper2"> 
+      <div>
+      Tempor voluptate  Veniam ut magna occaecat sit velit enim aliqua cupidatat. Proident irure occaecat esse sint enim. Et non quis deserunt eu ut magna nostrud Lorem excepteur veniam.Tempor voluptate  Veniam ut magna occaecat sit velit enim aliqua cupidatat. Proident irure occaecat esse sint enim. Et non quis deserunt eu ut magna nostrud Lorem excepteur veniam.Tempor voluptate  Veniam ut magna occaecat sit velit enim aliqua cupidatat. Proident irure occaecat esse sint enim. Et non quis deserunt eu ut magna nostrud Lorem excepteur veniam.Tempor voluptate  Veniam ut magna occaecat sit velit enim aliqua cupidatat. Proident irure occaecat esse sint enim. Et non quis deserunt eu ut magna nostrud Lorem excepteur veniam.Tempor voluptate  Veniam ut magna occaecat sit velit enim aliqua cupidatat. Proident irure occaecat esse sint enim. Et non quis deserunt eu ut magna nostrud Lorem excepteur veniam.
+      </div>
+      <div>
+        Tempor voluptate  
+      </div>
+      <div>
+        Tempor voluptate  
+      </div>
+      <div>
+        Tempor voluptate  
+      </div>
+      <div>
+        Tempor voluptate  
+      </div>
+      <div>
+        Tempor voluptate  
+      </div>
+      <div>
+        Tempor voluptate  
+      </div>
+      <div>
+        Tempor voluptate  
+      </div>
+
+    </div>  */}
+
+      {/* <div className="wrapper3">
+
+        <div className="fila-par">
+          <div>&#9820;</div>
+          <div>&#9822;</div>
+          <div>&#9821;</div>
+          <div>&#9819;</div>
+          <div>&#9818;</div>
+          <div>&#9821;</div>
+          <div>&#9822;</div>
+          <div>&#9820;</div>
+        </div>
+        <div className="fila-impar">
+          <div>&#9823;</div>
+          <div>&#9823;</div>
+          <div>&#9823;</div>
+          <div>&#9823;</div>
+          <div>&#9823;</div>
+          <div>&#9823;</div>
+          <div>&#9823;</div>
+          <div>&#9823;</div>
+        </div>
+
+        <div className="fila-par">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="fila-impar">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="fila-par">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="fila-impar">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+
+        <div className="fila-par">
+          <div>&#9820;</div>
+          <div>&#9822;</div>
+          <div>&#9821;</div>
+          <div>&#9819;</div>
+          <div>&#9818;</div>
+          <div>&#9821;</div>
+          <div>&#9822;</div>
+          <div>&#9820;</div>
+        </div>
+        <div className="fila-impar">
+          <div>&#9823;</div>
+          <div>&#9823;</div>
+          <div>&#9823;</div>
+          <div>&#9823;</div>
+          <div>&#9823;</div>
+          <div>&#9823;</div>
+          <div>&#9823;</div>
+          <div>&#9823;</div>
+        </div>
+      </div> */}
+{/* 
+<div className="wrapper4"> 
+      <div className='item1'>
+        Tempor voluptate item1
+      </div>
+      <div className='item2'>
+        Tempor voluptate  item2
+      </div>
+      <div className='item3'>
+        Tempor voluptate  item3
+      </div>
+      <div className='item4'>
+        Tempor voluptate  item4
+      </div>
+
+    </div> */}
+    </>
   );
 }
 
